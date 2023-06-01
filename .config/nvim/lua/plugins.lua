2d525666d0cff75119ca8d90f49e3ef860386ddf
doohochang <doohochang.gmail.com>
2023-06-02 05:56:10 +0900

    add treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -35,4 +35,23 @@ require('packer').startup(function(use)
   }
 
   use 'jremmen/vim-ripgrep'
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update { with_sync = true }
+      ts_update()
+    end,
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = { "lua", "vim", "scala", "kotlin" },
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      }
+    end,
+  }
 end)
