b3e0d6e87e83cb99624d18cb65048cee88a99d54
doohochang <doohochang.gmail.com>
2023-06-06 07:29:02 +0900

    enable fast_wrap of autopairs

--- .config/nvim/lua/setting/autopairs.lua
+++ .config/nvim/lua/setting/autopairs.lua
@@ -3,7 +3,9 @@ return {
     local autopairs = require('nvim-autopairs')
     local cond = require('nvim-autopairs.conds')
 
-    autopairs.setup {}
+    autopairs.setup {
+      fast_wrap = {},
+    }
 
     -- Disables <CR> for round brackets in Scala.
     autopairs.get_rules('(')[1]:with_cr(cond.not_filetypes { 'scala', 'sbt' })
