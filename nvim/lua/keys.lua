c8ab42695bd2e81322e836692d8894baa85d5903
doohochang <doohochang@gmail.com>
2024-11-11 13:55:38 +0900

    add an alias for get fetch --all

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -4,7 +4,7 @@ local map = vim.keymap.set
 map('n', '<leader>n', function() require('nvim-tree.api').tree.toggle() end)
 
 -- Telescope
-map('n', '<leader>ff', function() require('telescope.builtin').find_files() end)
+map('n', '<leader>ff', function() require('telescope.builtin').find_files { hidden = true, no_ignore = true, } end)
 map('n', '<leader>fg', function() require('telescope.builtin').live_grep() end)
 map('n', '<leader>fb', function() require('telescope.builtin').buffers() end)
 map('n', '<leader>fh', function() require('telescope.builtin').help_tags() end)
